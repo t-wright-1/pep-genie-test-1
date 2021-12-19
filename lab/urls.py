977db458cd250ddcbcb219b1a5fe814c1519c37c
sad-man-111 <tomwr1ght@hotmail.co.uk>
2021-12-19 00:27:37 +0000

    added template for resources

---
+++ lab/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('resources',views.resources, name = 'resources')
+]
--- lab/views.py
+++ lab/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def resources(request):
+    return render(request, 'lab/resources.html')
